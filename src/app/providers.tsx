1469397b8d8159bf240cd7bb4cfa20d943c8b3cc
samuelrms <samuelaoliveiraramos@gmail.com>
2023-08-21 14:55:19 -0300

    refactor(providers.tsx): adding cache to the provider
    Changing the provider to insert the @emotion/styled cache
    
    BREAKING CHANGE: Global application theme

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,10 +2,34 @@
 
 import { theme } from '@/base_styles/theme'
 import { Children } from '@/types/children'
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider, CacheProvider } from '@emotion/react'
+import { useState } from 'react'
+import createCache from '@emotion/cache'
+import { useServerInsertedHTML } from 'next/navigation'
 
 const Providers = ({ children }: Children) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>
+  const [cache] = useState(() => {
+    const cache = createCache({ key: 'css' })
+    cache.compat = true
+    return cache
+  })
+
+  useServerInsertedHTML(() => {
+    return (
+      <style
+        data-emotion={`${cache.key} ${Object.keys(cache.inserted).join(' ')}`}
+        dangerouslySetInnerHTML={{
+          __html: Object.values(cache.inserted).join(' '),
+        }}
+      />
+    )
+  })
+
+  return (
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </CacheProvider>
+  )
 }
 
 export default Providers
