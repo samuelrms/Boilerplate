9150fdbbe6fe127cc242b3e546fa53239c8b5045
samuelrms <samuelaoliveiraramos@gmail.com>
2023-08-21 14:52:36 -0300

    feat(theme): change styled-components to @emotion/styled
    change made due to the inability to cache the theme improving performance

--- src/base_styles/theme.ts
+++ src/base_styles/theme.ts
@@ -3,7 +3,6 @@ import { fonts } from './fonts'
 import { borderWidth, breakpoints, spacing } from './spacing'
 
 export const theme = {
-  name: 'default',
   ...colors,
   ...fonts,
   spacing: { ...spacing },
