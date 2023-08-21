4121b6bc0423000d632d8ea05f4b1676d7942bb5
samuelrms <samuelaoliveiraramos@gmail.com>
2023-08-21 11:01:02 -0300

    fix: refactoring the theme template
    change the way colors, fonts, spacing, breakpoint and borderWidth are used

--- src/base_styles/theme.ts
+++ src/base_styles/theme.ts
@@ -4,11 +4,11 @@ import { borderWidth, breakpoints, spacing } from './spacing'
 
 export const theme = {
   name: 'default',
-  colors,
-  fonts,
-  spacing,
-  breakpoints,
-  borderWidth,
+  ...colors,
+  ...fonts,
+  spacing: { ...spacing },
+  ...breakpoints,
+  ...borderWidth,
 }
 
 export type Theme = typeof theme
