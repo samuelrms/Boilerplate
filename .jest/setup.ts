dda5f3f0836ac46c1b68cb1b337340f93c2f2d02
samuelrms <samuelaoliveiraramos@gmail.com>
2023-08-23 14:07:53 -0300

    feat: add jest and testing library
    Configuration for running tests with jest and testing library

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,3 +1,7 @@
 import { matchers } from '@emotion/jest';
+import '@testing-library/jest-dom';
+import '@testing-library/react';
+import React from 'react';
 
+global.React = React;
 expect.extend(matchers);
--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,13 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/'],
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
 };
